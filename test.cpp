00602ee85214fad5f5f3299c13270798a7f1dfc2
Bora <boraozgen@gmail.com>
2021-03-09 21:52:22 +0100

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#define MBEDTLS_ENTROPY_NV_SEED
+#define MBED_CONF_RTOS_PRESENT 1
+
+#include "Callback.h"
+#include "netsocket/TLSSocket.h"
+
+int main()
+{
+    TLSSocket tlsSocket;
+    return 0;
+}
